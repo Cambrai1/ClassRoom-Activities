4dab72aff204cc244b0c137f3905831ba9af4433
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2017-10-10 15:02:53 +0100

    Added Power Jumps and Slider showing Power
    Added max + min jumps and chanded A-D from rotation to position

---
+++ ClassRoom Activities/Assets/ColorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ColorScript : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 m_customPosition;
+
+    public Renderer rend;
+
+    void Start()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = Color.red;
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            
+            
+        }
+        transform.position = m_customPosition;
+    }
+}
+
